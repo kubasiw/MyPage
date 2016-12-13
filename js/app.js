b52ec4c3dd84247045eb0370d73a38a8f1bfbe38
Jakub Siwiński <kubsiw@gmail.com>
2016-12-13 12:46:22 +0100

    navigation upgraded

--- js/app.js
+++ js/app.js
@@ -15,44 +15,55 @@ jQuery(document).ready(function(){
     var bLetter = jQuery('.bBox span');
     var creative = jQuery('.beBox span');
     
+    // menu C vars
+    var circle3 = jQuery('.circle3');
+    var cBox = jQuery('.cBox');
+    var ceBox = jQuery('.ceBox');
+    var cLetter = jQuery('.cBox span');
+    var contact = jQuery('.ceBox span');
+    
     function showMenu(){
         
         // jQuery for A
         
-        circle1.on('mouseenter', function(){
+        circle1.on('mouseover', function(){
             aLetter.css('left', '13.2'+'%')
+                   .css('color', 'black')
                    .css('transition', '500ms');
-            aboutBox.css('opacity', '1')
-                    .css('transition', '500ms')
-            jQuery(this).css('background-color', '#2D31FF')
+            aboutBox.css('display', 'inline-block')
+                    .css('opacity', '1')
+                    .css('transition', '500ms');
+            jQuery(this).css('background-color', '#5C67DA')
                         .css('transition', '500ms');
         });
         
         circle1.on('mouseout', function(){
             aLetter.css('left', '15.78'+'%')
+                   .css('color', 'white')
                    .css('transition', '500ms');
-            aboutBox.css('opacity', '0')
-                   .css('transition', '500ms');
-            circle1.css('background-color', 'dimgrey')
-                   .css('transition', '500ms');
-        });
-        
-        
-        aBox.on('mouseenter', function(){
-            aLetter.css('left', '13.2'+'%')
-                   .css('transition', '500ms');
-            aboutBox.css('opacity', '1')
-                    .css('transition', '500ms');
-            circle1.css('background-color', '#2D31FF')
+            aboutBox.css('display', 'none');
+            circle1.css('background-color', '#515151')
                    .css('transition', '500ms');
         });
         
-        aboutBox.on('mouseenter', function(){
+        
+        aBox.on('mouseover', function(){
             aLetter.css('left', '13.2'+'%')
                    .css('transition', '500ms');
-            aboutBox.css('opacity', '1')
+            aboutBox.css('display', 'inline-block')
+                    .css('opacity', '1')
                     .css('transition', '500ms');
-            circle1.css('background-color', '#2D31FF')
+            circle1.css('background-color', '#5C67DA')
+                   .css('transition', '500ms');
+        });
+        
+        aboutBox.on('mouseover', function(){
+            aLetter.css('left', '13.2'+'%')
+                   .css('transition', '500ms');
+            aboutBox.css('display', 'inline-block')
+                    .css('opacity', '1')
+                    .css('transition', '500ms');
+            circle1.css('background-color', '#5C67DA')
                    .css('transition', '500ms');
         });
         
@@ -60,40 +71,89 @@ jQuery(document).ready(function(){
         // jQuery for B
         
         
-        circle2.on('mouseenter', function(){
-            bLetter.css('left', '46.45'+'%')
+        circle2.on('mouseover', function(){
+            bLetter.css('left', '46.55'+'%')
+                   .css('color', 'black')
                    .css('transition', '500ms');
-            beBox.css('opacity', '1')
-                    .css('transition', '500ms')
-            jQuery(this).css('background-color', '#2D31FF')
+            beBox.css('display', 'inline-block')
+                 .css('opacity', '1')
+                 .css('transition', '500ms');
+            jQuery(this).css('background-color', '#975CDA')
                         .css('transition', '500ms');
         });
         
         circle2.on('mouseout', function(){
             bLetter.css('left', '49.17'+'%')
+                   .css('color', 'white')
                    .css('transition', '500ms');
-            beBox.css('opacity', '0')
+            beBox.css('display', 'none');
+            circle2.css('background-color', '#6D6D6D')
                    .css('transition', '500ms');
-            circle2.css('background-color', 'dimgrey')
+        });
+        
+        
+        bBox.on('mouseover', function(){
+            bLetter.css('left', '46.55'+'%')
+                   .css('transition', '500ms');
+            beBox.css('display', 'inline-block')
+                    .css('opacity', '1')
+                    .css('transition', '500ms');
+            circle2.css('background-color', '#975CDA')
+                   .css('transition', '500ms');
+        });
+        
+        beBox.on('mouseover', function(){
+            bLetter.css('left', '46.55'+'%')
+                   .css('transition', '500ms');
+            beBox.css('display', 'inline-block')
+                 .css('opacity', '1')
+                 .css('transition', '500ms');
+            circle2.css('background-color', '#975CDA')
                    .css('transition', '500ms');
         });
         
         
-        bBox.on('mouseenter', function(){
-            bLetter.css('left', '46.45'+'%')
+        // jQuery for C
+        
+        
+        circle3.on('mouseover', function(){
+            cLetter.css('left', '79.85'+'%')
+                   .css('color', 'black')
                    .css('transition', '500ms');
-            beBox.css('opacity', '1')
-                    .css('transition', '500ms');
-            circle2.css('background-color', '#2D31FF')
+            ceBox.css('display', 'inline-block')
+                 .css('opacity', '1')
+                 .css('transition', '500ms');
+            jQuery(this).css('background-color', '#5CDA9B')
+                        .css('transition', '500ms');
+        });
+        
+        circle3.on('mouseout', function(){
+            cLetter.css('left', '82.47'+'%')
+                   .css('color', 'white')
+                   .css('transition', '500ms');
+            ceBox.css('display', 'none');
+            circle3.css('background-color', '#8C8C8C')
                    .css('transition', '500ms');
         });
         
-        beBox.on('mouseenter', function(){
-            bLetter.css('left', '46.45'+'%')
+        
+        cBox.on('mouseover', function(){
+            cLetter.css('left', '79.85'+'%')
                    .css('transition', '500ms');
-            beBox.css('opacity', '1')
+            ceBox.css('display', 'inline-block')
+                    .css('opacity', '1')
                     .css('transition', '500ms');
-            circle2.css('background-color', '#2D31FF')
+            circle3.css('background-color', '#5CDA9B')
+                   .css('transition', '500ms');
+        });
+        
+        ceBox.on('mouseover', function(){
+            cLetter.css('left', '79.85'+'%')
+                   .css('transition', '500ms');
+            ceBox.css('display', 'inline-block')
+                 .css('opacity', '1')
+                 .css('transition', '500ms');
+            circle3.css('background-color', '#5CDA9B')
                    .css('transition', '500ms');
         });
         
